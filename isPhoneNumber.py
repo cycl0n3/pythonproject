1c1a075176366b77b0caa9e12aab89ba67787620
Milind <7milind7@gmail.com>
2017-10-03 01:16:59 +0530

    normal changes

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,6 +1,7 @@
 import re
 
-def isPhoneNumber(text):
+
+def is_phone_number(text):
     if len(text) != 12:
         return False
     for i in range(0, 3):
@@ -23,7 +24,7 @@ message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office'
 
 for i in range(0, len(message)):
     chunk = message[i:i+12]
-    if isPhoneNumber(chunk):
+    if is_phone_number(chunk):
         print('Phone number found: ' + chunk)
 
 print('-'*20)
