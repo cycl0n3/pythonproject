f53c0905f4b7a3cdef81ee89fd0d90b2624fa8fd
Milind <7milind7@gmail.com>
2017-10-04 12:49:29 +0530

    some refactors

---
+++ regex2.py
@@ -0,0 +1,16 @@
+import re
+
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('Cell: 415-555-9999 Work: 212-555-0000')
+print(mo.group())
+print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
+
+print('-'*20)
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
+
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge'))
+
