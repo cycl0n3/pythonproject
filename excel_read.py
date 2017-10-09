b782749330146a8764c9cdf1a18cb35f53349079
Milind <7milind7@gmail.com>
2017-10-09 13:32:32 +0530

    excel sheets census

--- excel_read.py
+++ excel_read.py
@@ -1,5 +1,6 @@
 import openpyxl
-import bleach
+from openpyxl.utils import get_column_letter, column_index_from_string
+
 
 wb = openpyxl.load_workbook('data/example.xlsx')
 
@@ -20,3 +21,35 @@ print(c.value)
 print('Row ' + str(c.row) + ', Column ' + c.column + ' is ' + c.value)
 print('Cell ' + c.coordinate + ' is ' + c.value)
 print(sheet['C1'].value)
+
+print('-'*20)
+
+print(sheet.cell(row=1, column=2))
+print(sheet.cell(row=1, column=2).value)
+for i in range(1, 8, 2):
+    print(i, sheet.cell(row=i, column=2).value)
+
+print('-'*20)
+
+print(get_column_letter(1))
+print(get_column_letter(2))
+print(get_column_letter(27))
+print(get_column_letter(900))
+print(get_column_letter(sheet.max_column))
+print(get_column_letter(sheet.max_row))
+
+print('-'*20)
+
+print(sheet['A1': 'C3'])
+print('')
+
+for rowOfCellObjects in sheet['A1':'C3']:
+    for cellObj in rowOfCellObjects:
+        print(cellObj.coordinate, cellObj.value)
+    print('--- END OF ROW ---')
+
+print('-'*20)
+
+for cellObj in list(sheet.columns)[1]:
+    print(cellObj.value)
+
