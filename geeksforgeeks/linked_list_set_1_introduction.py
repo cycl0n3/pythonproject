a72c8b75ec776d08df69fb0cd26b74b890fba4f0
Milind <7milind7@gmail.com>
2017-10-10 12:49:44 +0530

    data structures

--- geeksforgeeks/linked_list_set_1_introduction.py
+++ geeksforgeeks/linked_list_set_1_introduction.py
@@ -1,5 +1,29 @@
-class Node:
+class Node(object):
     def __init__(self, data):
         self.data = data
         self.next = None
 
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+
+if __name__ == '__main__':
+    list = LinkedList()
+
+    list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    list.head.next = second
+    second.next = third
+
+    list.print_list()
---
+++ geeksforgeeks/linked_list_set_2_inserting_a_node.py
@@ -0,0 +1,41 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print('The given previous node must in LinkedList.')
+            return
+
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last = self.head
+
+        while last.next:
+            last = last.next
+
+        last.next = new_node
+
+
+if __name__ == '__main__':
+    pass
