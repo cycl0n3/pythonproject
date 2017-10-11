ae3cdd46732ad162b522a6bf40900b9b7bf11b91
Milind <7milind7@gmail.com>
2017-10-11 12:15:33 +0530

    linked lists

--- geeksforgeeks/find_length_of_a_linked_list_iterative_and_recursive.py
+++ geeksforgeeks/find_length_of_a_linked_list_iterative_and_recursive.py
@@ -15,6 +15,36 @@ class LinkedList(object):
             print(temp.data)
             temp = temp.next
 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+
+        self.head = new_node
+
+    def getCount(self):
+        temp = self.head
+        count = 0
+
+        while temp:
+            count += 1
+            temp = temp.next
+
+        return count
+
+    def getCountRec(self, node):
+        if not node:
+            return 0
+        else:
+            return 1 + self.getCountRec(node.next)
+
 
 if __name__ == '__main__':
-    pass
+    list = LinkedList()
+    list.push(1)
+    list.push(3)
+    list.push(1)
+    list.push(2)
+    list.push(1)
+
+    print('The count of nodes is: ', list.getCount())
+    print('The count of nodes is: ', list.getCountRec(list.head))
