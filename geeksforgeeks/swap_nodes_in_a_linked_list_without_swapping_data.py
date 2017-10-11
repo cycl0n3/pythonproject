68a352a3a3279211aa3cb7ef7b684a43588120e8
Milind <7milind7@gmail.com>
2017-10-11 12:31:12 +0530

    linked lists

---
+++ geeksforgeeks/swap_nodes_in_a_linked_list_without_swapping_data.py
@@ -0,0 +1,26 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+
+        self.head = new_node
+
+
+if __name__ == '__main__':
+    pass
