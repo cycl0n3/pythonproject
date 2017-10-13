2af7c9ec51f473f52536a27a3497eaa2f4ae1924
Milind <7milind7@gmail.com>
2017-10-13 15:51:45 +0530

    linked lists

--- geeksforgeeks/write_a_function_to_get_nth_node_in_a_linked_list.py
+++ geeksforgeeks/write_a_function_to_get_nth_node_in_a_linked_list.py
@@ -15,6 +15,35 @@ class LinkedList(object):
             print(temp.data)
             temp = temp.next
 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+
+        self.head = new_node
+
+    def getNth(self, index):
+        current = self.head
+        count = 0
+
+        while current:
+            if count == index:
+                return current.data
+            count += 1
+            current = current.next
+
+        assert False
+
 
 if __name__ == '__main__':
-    pass
+    llist = LinkedList()
+
+    llist.push(1)
+    llist.push(4)
+    llist.push(1)
+    llist.push(12)
+    llist.push(1)
+
+    n = 3
+
+    print("Element at index 3 is :", llist.getNth(n))
+
